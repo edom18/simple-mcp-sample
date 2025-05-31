e34bd292d12dd5d1b088656d2e39d7d8f651b0f9
edom18 <edo.m18@gmail.com>
2025-05-31 13:09:10 +0900

    config: Changed arguments.

--- multi-client-launch-demo.sh
+++ multi-client-launch-demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python client/multi_client.py simple_server server/simple_server.py
+python client/multi_client.py simple_server server/simple_server.py profile_data_server server/data_server.py
